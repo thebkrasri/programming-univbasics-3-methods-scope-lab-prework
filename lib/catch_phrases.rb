abf5e9c47fbdfe8fea16d1815ed491f67622408d
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 18:09:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = ""
+  phrase = "It's-a me, Mario"
   puts phrase
 end
 
 def toadstool
+  status = 
   puts status
 end
